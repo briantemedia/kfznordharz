23c444ef7e3c2d4a825bf6c7c319bf49e7e55ac3
briantemedia <briantemedia@gmail.com>
2025-04-01 17:02:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,14 +26,4 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   updateSlider();
-
-  // Header-Hintergrund beim Scrollen anpassen:
-  window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
 });
